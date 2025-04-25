03519b81c3fe1f9debd0608422391df90f67a836
Alex Wang <alxswang@umich.edu>
2025-04-25 21:00:02 +0800

    added gunicorn

--- backend/app.py
+++ backend/app.py
@@ -252,7 +252,7 @@ def generate_schedules():
     return jsonify(flattened_schedules), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
 
 # @app.route('/api/generate-schedules-old', methods=['POST'])
