c392bc4090cf979040f60a8d9cb5dc886df99f37
Alex Wang <alxswang@umich.edu>
2025-04-25 20:46:17 +0800

    backend changes for deployment

--- backend/app.py
+++ backend/app.py
@@ -252,7 +252,7 @@ def generate_schedules():
     return jsonify(flattened_schedules), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)
 
 
 # @app.route('/api/generate-schedules-old', methods=['POST'])
